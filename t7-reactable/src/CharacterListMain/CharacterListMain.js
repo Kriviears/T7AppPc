651b533e1a86a230b2378d28286c05c375e0ee90
kriviears <kriviears@gmail.com>
2020-02-12 12:03:34 -0500

    few changes to routing before Zeit

--- t7-reactable/src/CharacterListMain/CharacterListMain.js
+++ t7-reactable/src/CharacterListMain/CharacterListMain.js
@@ -1,48 +1,41 @@
-import React, { Component } from 'react'
-import './CharacterListMain.css'
-import { Link } from 'react-router-dom'
-import CharacterCard from '../CharacterCard/CharacterCard'
-import TekkenContext from '../TekkenContext';
-import CharApiService from '../services/char-api-service';
+import React, { Component } from "react";
+import "./CharacterListMain.css";
+import { Link } from "react-router-dom";
+import CharacterCard from "../CharacterCard/CharacterCard";
+import TekkenContext from "../TekkenContext";
+import CharApiService from "../services/char-api-service";
 
-export default class CharacterListMain extends Component{
-    static contextType = TekkenContext
+export default class CharacterListMain extends Component {
+  static contextType = TekkenContext;
 
-    
+  componentDidMount() {
+    CharApiService.getCharacters().then(data => {
+      this.context.setCharacters(data);
+    });
+  }
 
-    componentDidMount(){
-        CharApiService.getCharacters()
-            .then(data=>{
-                this.context.setCharacters(data);
-            });
-    }
+  render() {
+    const { characters, characterSelect } = this.context;
+    let filteredList = characters;
 
-    render(){
-        const { characters, characterSelect } = this.context
-        let filteredList = characters
-
-        return(
-            <div className='CharListMain'>
-                <ul className='CharList'>
-                    {filteredList.map((char, i) =>
-                        <li 
-                        key={i}
-                        onClick={() => characterSelect(i)}
-                        >
-                            <Link
-                                className='CharacterList__link'
-                                to={`/character/${char.name}`}>
-                                <CharacterCard 
-                                    name={char.name}
-                                    src={char.thumbnail}
-                                    alt={char.name}
-                                />
-                            </Link>
-                        </li>
-                
-                    )}
-                </ul>
-            </div>
-        )
-    }
+    return (
+      <div className="CharListMain">
+        <ul className="CharList">
+          {filteredList.map((char, i) => (
+            <li key={i} onClick={() => characterSelect(i)}>
+              <Link
+                className="CharacterList__link"
+                to={`/character/${char.name}/framedata`}>
+                <CharacterCard
+                  name={char.name}
+                  src={char.thumbnail}
+                  alt={char.name}
+                />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
